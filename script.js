e307a62b6de625fdc7d5c77976aa82d13b7e6360
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-07-01 01:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', () => {
+            removeActiveClasses();
+            panel.classList.add('active');
+        });
+    });
+
+    function removeActiveClasses() {
+        panels.forEach(panel => {
+            panel.classList.remove('active');
+        });
+    }
+});
